8fa8f325a188292e099f40593ca1d8deeb787b59
Thalavaimanikandan <146534925+Thalavaimanikandan@users.noreply.github.com>
2025-09-12 11:11:21 +0530

    Add files via upload
    Traceback (most recent call last):
      File "/home/thalavai-manikandan/Desktop/json/json_parser.py", line 81, in <module>
        searchable_json = JSONSearchable.parse(json_data)
                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/thalavai-manikandan/Desktop/json/json_parser.py", line 9, in parse
        return JSONSearchable (eval(json_string,{ "builtins":None },{}))
                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "<string>", line 2
        {
    IndentationError: unexpected indent

---
+++ json_parser.py
@@ -0,0 +1,93 @@
+import re        
+class JSONSearchable:
+    def __init__(self, data):
+        self.data=data
+    
+    def parse(json_string):
+        for old,new in [("true","True"),("false","False"),("null","None")]:
+         json_string=json_string.replace(old,new)
+         return JSONSearchable (eval(json_string,{ "builtins":None },{}))
+       
+    def search(self,path):
+        parts=re.split(r'\.(?![^\[]*\])',path)
+        current_data=self.data
+        for value in parts:
+            if not value:
+                return None
+            if "[" in value:
+                key,*rest=re.split(r'(\[.*?\])',value) 
+                if key:
+                    current_data=current_data.get(key)
+                    if isinstance:
+                        return current_data,dict
+                    else:
+                        return None
+                for r in rest:
+                    if not r:
+                        continue
+                    if r.startswith("[?"):
+                        obj,op,val=re.split(r'(==,!=,>,<,>=,<=)') 
+                        obj,value=obj.strip(),value.strip().strip('"') 
+                        res=[]
+                        for item in current_data:
+                            if isinstance:
+                                return current_data,list
+                            else:
+                                return res
+                                if obj in item:
+                                    right=float(value)
+                                    if value:
+                                      value.replace('.','',1).isdigit()
+                                    elif value=="true":
+                                        return True
+                                    elif value=="false":
+                                        return False
+                                    elif value=="null":
+                                        return None
+                                    else:
+                                        return value
+                                    if eval(f"item[obj] {op} right"):
+                                        res.append(item)
+                        current_data= res          
+                    else:
+                        idx=int(r[1:-1])
+                        current_data=current_data[idx]
+                        if isinstance:
+                            return current_data,list
+                        else:
+                            return None
+            else:
+                current_data=current_data.get(value)  
+                if isinstance:
+                    return current_data,dict
+                else:
+                    return None
+        return current_data                       
+
+if __name__=="__main__":
+    json_data = """
+    {
+        "store": "Main Street Books",
+        "inventory": [
+        { "type": "book", "title": "The Great Gatsby", "price": 12.50, "in_stock": true },
+        { "type": "book", "title": "Moby Dick", "price": 15.00, "in_stock": false },
+        { "type": "magazine", "title": "Tech Today", "price": 5.99, "in_stock": true }
+        ],
+        "location": {
+        "city": "New York",
+        "postcode": "10001"
+    }TypeError: JSONSearchable.search() missing 1 required positional argument: 'path'
+    """
+    searchable_json = JSONSearchable.parse(json_data)
+
+    Queries=[
+        "store",
+        "inventory[0].title",
+        "inventory[?in_stock==true]",
+        "inventory[?price<15.0].title",
+        "location.country"
+    ]
+    print(Queries)
+
+    print(searchable_json.search(Queries))
+
